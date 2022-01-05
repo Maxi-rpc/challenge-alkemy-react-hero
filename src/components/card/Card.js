548492f3fee2d94f9c0c7a8ee2a230424635133e
Maximiliano <maxirpc2607@gmail.com>
2022-01-05 08:36:04 -0300

    Add Search page and card

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,17 +1,18 @@
-export default function Card() {
-  return (
-    <>
-      <div class="card bg-dark text-white">
-        <img src="..." class="card-img" alt="..." />
-        <div class="card-img-overlay">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This content is a little bit longer.
-          </p>
-          <p class="card-text">Last updated 3 mins ago</p>
-        </div>
-      </div>
-    </>
-  );
+export default function Card({ hero }) {
+	console.log(hero);
+	return (
+		<>
+			<div className="card bg-dark text-white">
+				<img src={hero.image.url} className="card-img" alt={hero.name} />
+				<div className="card-img-overlay">
+					<h5 className="card-title">Card title</h5>
+					<p className="card-text">
+						This is a wider card with supporting text below as a natural lead-in
+						to additional content. This content is a little bit longer.
+					</p>
+					<p className="card-text">Last updated 3 mins ago</p>
+				</div>
+			</div>
+		</>
+	);
 }
